e7782f80f56b58fba61f10ea876211d0b1c07aca
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-15 13:52:51 +0300

    https://www.patika.dev/tr

--- src/TriangleStar.java
+++ src/TriangleStar.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class TriangleStar {
+public class TriangleStar {
+    public static void main(String[] args) {
+        int number =10;
+
+        for (int i=0;i<=number;i++){
+
+            for(int j=1;j<=i;j++) {
+                System.out.print(" ");
+
+            }
+            for (int k=1;k<=((2*number)-(2*i-1));k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
